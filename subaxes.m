1cd7f479a21b866f57d44a7d800ec1b41e5eec74
Oliver Kiethe <oliverkiethe@web.de>
2018-06-17 16:37:45 +0200

    improved subaxes

--- subaxes.m
+++ subaxes.m
@@ -10,10 +10,15 @@ function h = subaxes(m, n, p)
  
 h = subplot(m, n, p);
 
-outerPosition(1) = mod(p-1, n)/n;
-outerPosition(2) = 1-(floor((p-1)/n)+1)/m;
-outerPosition(3) = 1/n;
-outerPosition(4) = 1/m;
+p = p - 1;
+
+pM = 1 + max(floor(p/n)) - min(floor(p/n));
+pN = 1 + max(mod(p, n)) - min(mod(p, n));
+
+outerPosition(1) = mod(min(p), n)/n;
+outerPosition(2) = 1-(floor((max(p))/n)+1)/m;
+outerPosition(3) = 1/n*pN;
+outerPosition(4) = 1/m*pM;
 
 set(h, 'OuterPosition', outerPosition);
 
